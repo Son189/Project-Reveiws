35c8c281b7b7a6aed1ccc0b4210373338656cb26
son189 <son.august.7334@gmail.com>
2023-02-27 10:45:47 +0300

    done

--- models/product.rb
+++ models/product.rb
@@ -3,16 +3,11 @@ class Product < ActiveRecord::Base
     has_many :users, through: :reviews
   
 def leave_review(user,star_rating,comment)
-
-
+review = Review.new(user: name:,star_rating: star_rating,comment: comment, product: self)
 end
 def print_all_reviews
 puts " Review for #{insert product name} by #{insert user name}: #{insert review star_rating}. #{insert review comment}"
 end
-def average_rating
-
-
-    end
-
-
+def average_rating 
+ end
 end
