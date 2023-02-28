34d2f09e0a150a73364b9715d3fc8a9228bd9f7d
son189 <son.august.7334@gmail.com>
2023-02-28 06:18:00 +0300

    updated products

--- models/product.rb
+++ models/product.rb
@@ -3,11 +3,23 @@ class Product < ActiveRecord::Base
     has_many :users, through: :reviews
   
 def leave_review(user,star_rating,comment)
-review = Review.new(user: name:,star_rating: star_rating,comment: comment, product: self)
+review = Review.create(user: user,star_rating: star_rating,comment: comment, product: self)
+reviews << review
 end
 def print_all_reviews
-puts " Review for #{insert product name} by #{insert user name}: #{insert review star_rating}. #{insert review comment}"
+    reviews.each do |review|
+puts " Review for #{name} by #{review.user.name}: #{ review.star_rating}. #{insert review comment}"
+end
 end
 def average_rating 
- end
+sum =0.0
+count = 0
+
+reviews.each do |r|
+    sum += r.star_rating
+    count += 1
+end
+(sum/count).to_f
+end
+
 end
